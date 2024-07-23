de28a1f0f82a8e08cc4daa39fb7dbbc4b9ae1d06
root <root@ubuntuSponsor.yyqc5ezppzrexbtkrx40r5kiib.ex.internal.cloudapp.net>
2024-07-23 17:25:49 +0000

    Add: include slash r sub regex

--- odc-json-report-to-markdown.py
+++ odc-json-report-to-markdown.py
@@ -14,6 +14,7 @@ def get_report_directory(path):
 
 def format_versions(text):
     text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
     pattern = r'(\b\d+\.\d+\.\d+\b)'
     formatted_text = re.sub(pattern, r'`\1`', text)
     return formatted_text
